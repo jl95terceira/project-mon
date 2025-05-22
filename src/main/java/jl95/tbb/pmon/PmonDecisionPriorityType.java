78d1eeb762deca36257e7f70675215f361d9a49d
João <jl95terceira@gmail.com>
2025-05-22 22:52:26 +0100

    remove unused

--- src/main/java/jl95/tbb/pmon/PmonDecisionPriorityType.java
+++
@@ -1,9 +0,0 @@
-package jl95.tbb.pmon;
-
-public enum PmonDecisionPriorityType {
-    MOVE(0),
-    SWITCH_IN(1);
-
-    public final Integer value;
-    PmonDecisionPriorityType(Integer value) {this.value = value;}
-}
