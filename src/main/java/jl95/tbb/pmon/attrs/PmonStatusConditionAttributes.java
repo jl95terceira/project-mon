e39b99bd31e15ce1b04f2d5fcfa321be4def37d4
João <jl95terceira@gmail.com>
2025-06-15 21:36:24 +0100

    simplify

--- src/main/java/jl95/tbb/pmon/attrs/PmonStatusConditionAttributes.java
+++ src/main/java/jl95/tbb/pmon/attrs/PmonStatusConditionAttributes.java
@@ -3,6 +3,7 @@ package jl95.tbb.pmon.attrs;
 import static jl95.lang.SuperPowers.Map;
 import static jl95.lang.SuperPowers.strict;
 
+import jl95.lang.variadic.Function0;
 import jl95.lang.variadic.Function1;
 import jl95.tbb.pmon.status.PmonStatModifierType;
 import jl95.util.StrictMap;
@@ -10,7 +11,7 @@ import jl95.util.StrictMap;
 public class PmonStatusConditionAttributes {
 
     public StrictMap<PmonStatModifierType, Double> statFactors = strict(Map());
-    public Function1<Double, Integer> cureChanceByTurn = turnNr_ -> 0.0;
-    public Function1<Double, Integer> immobiliseChanceByTurn = turnNr_ -> 0.0;
+    public Function0<Double> cureChance = () -> 0.0;
+    public Function0<Double> immobiliseChance = () -> 0.0;
     public Boolean allowDecide = true;
 }
