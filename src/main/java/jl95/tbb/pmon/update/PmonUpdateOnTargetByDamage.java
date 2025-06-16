a3547254cf144a517459da44676b77a330efe4bd
jl95terceira <jl95terceira@gmail.com>
2025-06-16 12:16:25 +0100

    .

---
+++ src/main/java/jl95/tbb/pmon/update/PmonUpdateOnTargetByDamage.java
@@ -0,0 +1,9 @@
+package jl95.tbb.pmon.update;
+
+public class PmonUpdateOnTargetByDamage {
+
+    public Integer damage = null;
+    public Integer healback = null;
+    public Double effectivenessFactor = 1.0; // facultative
+    public Boolean criticalHit = false; // facultative
+}
---
+++ src/main/java/jl95/tbb/pmon/update/PmonUpdateOnTargetByStatusCondition.java
@@ -0,0 +1,13 @@
+package jl95.tbb.pmon.update;
+
+import jl95.lang.StrictList;
+import jl95.tbb.pmon.status.PmonStatusCondition;
+import jl95.util.StrictSet;
+
+import static jl95.lang.SuperPowers.*;
+
+public class PmonUpdateOnTargetByStatusCondition {
+
+    public StrictList<PmonStatusCondition> statusConditionsApply = strict(List());
+    public StrictSet<PmonStatusCondition.Id> statusConditionsRemove = strict(Set());
+}
