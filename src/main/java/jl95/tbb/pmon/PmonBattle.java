9232f8817d5156224dacab41858a79fb128b106b
jl95terceira <jl95terceira@gmail.com>
2026-02-02 10:11:18 +0000

    refactor

--- src/main/java/jl95/tbb/pmon/PmonBattle.java
+++ src/main/java/jl95/tbb/pmon/PmonBattle.java
@@ -1,30 +1,52 @@
 package jl95.tbb.pmon;
 
+import java.util.Optional;
+
+import jl95.lang.variadic.Function0;
+import jl95.lang.variadic.Function1;
 import jl95.tbb.Battle;
+import jl95.tbb.PartyId;
 import jl95.tbb.mon.MonBattle;
+import jl95.tbb.mon.MonFieldPosition;
 import jl95.tbb.mon.MonRuleset;
 import jl95.tbb.pmon.update.PmonUpdate;
+import jl95.util.StrictMap;
+import jl95.util.StrictSet;
 
-public class PmonBattle extends MonBattle<
+public class PmonBattle {
+
+    private final PmonRuleset ruleset;
+    private final MonBattle<
         Pmon, PmonPartyEntry, PmonParty,
         PmonInitialConditions,
         PmonLocalContext,
         PmonGlobalContext,
         PmonDecision,
         PmonUpdate, PmonUpdate
-        > {
+        > upcastBattle;
 
     public interface Handler extends Battle.Handler<PmonUpdate, PmonLocalContext, PmonGlobalContext> {
 
-        static class Editable extends Battle.Handler.Editable<PmonUpdate, PmonLocalContext, PmonGlobalContext> implements Handler {}
-        static class Extendable extends Battle.Handler.Extendable<PmonUpdate, PmonLocalContext, PmonGlobalContext> implements Handler {}
+        class Editable extends Battle.Handler.Editable<PmonUpdate, PmonLocalContext, PmonGlobalContext> implements Handler {}
+        class Extendable extends Battle.Handler.Extendable<PmonUpdate, PmonLocalContext, PmonGlobalContext> implements Handler {}
         static Handler ignore() { return new Editable(); }
     }
 
-    public PmonBattle(MonRuleset<Pmon, PmonPartyEntry, PmonParty, PmonInitialConditions, PmonLocalContext, PmonGlobalContext, PmonDecision, PmonUpdate, PmonUpdate> ruleset) {
-        super(ruleset);
+    public PmonBattle(PmonRuleset ruleset) {
+        this.ruleset = ruleset;
+        this.upcastBattle = new MonBattle<>(ruleset);
     }
     public PmonBattle() {
         this(new PmonRuleset());
+    }
+
+    public Optional<PartyId> start(
+            StrictMap<PartyId, PmonPartyEntry> parties,
+            PmonInitialConditions initialConditions,
+            Function1<StrictMap<PartyId, StrictMap<MonFieldPosition, PmonDecision>>,
+                                  StrictMap<PartyId, StrictSet<MonFieldPosition>>> decisionFunction,
+            Battle.Handler<PmonUpdate, PmonLocalContext, PmonGlobalContext> handler,
+            Function0<Boolean> toInterrupt) {
+        return upcastBattle.start(parties, initialConditions, decisionFunction, handler, toInterrupt);
     }
 }
